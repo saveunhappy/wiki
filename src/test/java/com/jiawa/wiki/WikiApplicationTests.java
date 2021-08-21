755085aa32f441b57c94c027e9282748c80e89dc
JiangTaoHou <925147954@qq.com>
2021-08-21 16:48:13 +0800

    修改启动文案

--- src/test/java/com/jiawa/wiki/WikiApplicationTests.java
+++ src/test/java/com/jiawa/wiki/WikiApplicationTests.java
@@ -1,13 +1,15 @@
 package com.jiawa.wiki;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class WikiApplicationTests {
-
     @Test
     void contextLoads() {
+
     }
 
 }
