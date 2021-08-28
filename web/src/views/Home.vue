058ce6bc8e7f25249c4fc0b98ebb5298cccefae3
JiangTaoHou <925147954@qq.com>
2021-08-28 17:16:28 +0800

    集成图标组件 npm install @ant-design/icons-vue --save 电子书布局调整 一行显示三列电子书

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,90 +1,80 @@
 <template>
   <a-layout>
-   <a-layout-sider width="200" style="background: #fff">
-     <a-menu
-         mode="inline"
-         :style="{ height: '100%', borderRight: 0 }"
-     >
-       <a-sub-menu key="sub1">
-         <template #title>
+    <a-layout-sider width="200" style="background: #fff">
+      <a-menu
+          mode="inline"
+          :style="{ height: '100%', borderRight: 0 }"
+      >
+        <a-sub-menu key="sub1">
+          <template #title>
               <span>
-                <user-outlined />
+                <user-outlined/>
                 subnav 1111
               </span>
-         </template>
-         <a-menu-item key="1">option1</a-menu-item>
-         <a-menu-item key="2">option2</a-menu-item>
-         <a-menu-item key="3">option3</a-menu-item>
-         <a-menu-item key="4">option4</a-menu-item>
-       </a-sub-menu>
-       <a-sub-menu key="sub2">
-         <template #title>
+          </template>
+          <a-menu-item key="1">option1</a-menu-item>
+          <a-menu-item key="2">option2</a-menu-item>
+          <a-menu-item key="3">option3</a-menu-item>
+          <a-menu-item key="4">option4</a-menu-item>
+        </a-sub-menu>
+        <a-sub-menu key="sub2">
+          <template #title>
               <span>
-                <laptop-outlined />
+                <laptop-outlined/>
                 subnav 2
               </span>
-         </template>
-         <a-menu-item key="5">option5</a-menu-item>
-         <a-menu-item key="6">option6</a-menu-item>
-         <a-menu-item key="7">option7</a-menu-item>
-         <a-menu-item key="8">option8</a-menu-item>
-       </a-sub-menu>
-       <a-sub-menu key="sub3">
-         <template #title>
+          </template>
+          <a-menu-item key="5">option5</a-menu-item>
+          <a-menu-item key="6">option6</a-menu-item>
+          <a-menu-item key="7">option7</a-menu-item>
+          <a-menu-item key="8">option8</a-menu-item>
+        </a-sub-menu>
+        <a-sub-menu key="sub3">
+          <template #title>
               <span>
-                <notification-outlined />
+                <notification-outlined/>
                 subnav 3
               </span>
-         </template>
-         <a-menu-item key="9">option9</a-menu-item>
-         <a-menu-item key="10">option10</a-menu-item>
-         <a-menu-item key="11">option11</a-menu-item>
-         <a-menu-item key="12">option12</a-menu-item>
-       </a-sub-menu>
-     </a-menu>
-   </a-layout-sider>
-   <a-layout-content
-       :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
-   >
-     <a-list item-layout="vertical" size="large" :pagination="pagination" :data-source="listData">
-       <template #footer>
-         <div>
-           <b>ant design vue</b>
-           footer part
-         </div>
-       </template>
-       <template #renderItem="{ item }">
-         <a-list-item key="item.title">
-           <template #actions>
+          </template>
+          <a-menu-item key="9">option9</a-menu-item>
+          <a-menu-item key="10">option10</a-menu-item>
+          <a-menu-item key="11">option11</a-menu-item>
+          <a-menu-item key="12">option12</a-menu-item>
+        </a-sub-menu>
+      </a-menu>
+    </a-layout-sider>
+    <a-layout-content
+        :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
+    >
+      <a-list item-layout="vertical" size="large" :grid="{gutter: 20,column : 3}" :data-source="ebooks">
+
+        <template #renderItem="{ item }">
+          <a-list-item key="item.name">
+            <template #actions>
           <span v-for="{ type, text } in actions" :key="type">
-            <component v-bind:is="type" style="margin-right: 8px" />
+            <component v-bind:is="type" style="margin-right: 8px"/>
             {{ text }}
           </span>
-           </template>
-           <template #extra>
-             <img
-                 width="272"
-                 alt="logo"
-                 src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
-             />
-           </template>
-           <a-list-item-meta :description="item.description">
-             <template #title>
-               <a :href="item.href">{{ item.title }}</a>
-             </template>
-             <template #avatar><a-avatar :src="item.avatar" /></template>
-           </a-list-item-meta>
-           {{ item.content }}
-         </a-list-item>
-       </template>
-     </a-list>
+            </template>
 
-   </a-layout-content>
-    </a-layout>
+            <a-list-item-meta :description="item.description">
+              <template #title>
+                <a :href="item.href">{{ item.name }}</a>
+              </template>
+              <template #avatar>
+                <a-avatar :src="item.cover"/>
+              </template>
+            </a-list-item-meta>
+          </a-list-item>
+        </template>
+      </a-list>
+
+    </a-layout-content>
+  </a-layout>
 </template>
 
 <script lang="ts">
-import { defineComponent,onMounted,ref,reactive,toRef} from 'vue';
+import {defineComponent, onMounted, ref, reactive, toRef} from 'vue';
 import axios from 'axios';
 
 const listData: any = [];
@@ -104,12 +94,12 @@ for (let i = 0; i < 23; i++) {
 
 export default defineComponent({
   name: 'Home',
-  setup(){
+  setup() {
     console.log("setup");
     const ebooks = ref();
-    const ebooks1 = reactive({books:[]});
+    const ebooks1 = reactive({books: []});
 
-    onMounted(()=>{
+    onMounted(() => {
       console.log("onMounted");
       axios.get("http://localhost:8880/ebook/list?name=Spring").then((response) => {
         const data = response.data;
@@ -118,20 +108,20 @@ export default defineComponent({
         console.log(response);
       });
     });
-    return{
+    return {
       ebooks,
-      ebooks2:toRef(ebooks1,"books"),
+      ebooks2: toRef(ebooks1, "books"),
       listData,
-      pagination:{
-        onChange:(page: any) =>{
+      pagination: {
+        onChange: (page: any) => {
           console.log(page);
         },
-        pageSize:3,
+        pageSize: 3,
       },
-      actions:[
-        {type:'StarOutlined',text:'156'},
-        {type:'LikeOutlined',text:'156'},
-        {type:'MessageOutlined',text:'2'},
+      actions: [
+        {type: 'StarOutlined', text: '156'},
+        {type: 'LikeOutlined', text: '156'},
+        {type: 'MessageOutlined', text: '2'},
       ],
     }
   }
