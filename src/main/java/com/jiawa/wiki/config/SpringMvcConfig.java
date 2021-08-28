66826d3fa7c803bf350520e6797f7421405c76b2
JiangTaoHou <925147954@qq.com>
2021-08-28 20:59:42 +0800

    增加拦截器，打印接口耗时

---
+++ src/main/java/com/jiawa/wiki/config/SpringMvcConfig.java
@@ -0,0 +1,21 @@
+package com.jiawa.wiki.config;
+
+import com.jiawa.wiki.interceptor.LoginInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
+
+@Configuration
+public class SpringMvcConfig implements WebMvcConfigurer {
+
+    @Resource
+    LoginInterceptor loginInterceptor;
+
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/login");
+    }
+}
