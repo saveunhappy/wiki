2c5ddfeb78bca042c2db8131d5a3b89c29fc67e2
JiangTaoHou <925147954@qq.com>
2021-08-30 01:02:03 +0800

    为保存接口增加参数校验

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -76,7 +76,7 @@ export default defineComponent({
     const ebooks = ref();
     const pagination = ref({
       current: 1,
-      pageSize: 4,
+      pageSize: 10,
       total: 0
     });
     const loading = ref(false);
@@ -162,18 +162,17 @@ export default defineComponent({
     const handleModalOk = () => {
       modalLoading.value = true;
       axios.post("/ebook/save", ebook.value).then((response) => {
-
+        modalLoading.value = false;
         const data = response.data;//data = commonResponse
         if (data.success) {
-          modalLoading.value = false;
           modalVisible.value = false;
-
           //重新加载列表
-
           handleQuery({
             page: pagination.value.current,
             size: pagination.value.pageSize
           });
+        }else {
+          message.error(data.message);
         }
       });
     }
