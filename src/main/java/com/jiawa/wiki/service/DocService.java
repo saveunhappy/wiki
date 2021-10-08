3de8d2df249f9f66b95ce9bb034581f536907caf
JiangTaoHou <925147954@qq.com>
2021-10-08 21:57:20 +0800

    增加保存文档功能

--- src/main/java/com/jiawa/wiki/service/DocService.java
+++ src/main/java/com/jiawa/wiki/service/DocService.java
@@ -2,8 +2,10 @@ package com.jiawa.wiki.service;
 
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
+import com.jiawa.wiki.domain.Content;
 import com.jiawa.wiki.domain.Doc;
 import com.jiawa.wiki.domain.DocExample;
+import com.jiawa.wiki.mapper.ContentMapper;
 import com.jiawa.wiki.mapper.DocMapper;
 import com.jiawa.wiki.req.DocQueryReq;
 import com.jiawa.wiki.req.DocSaveReq;
@@ -26,7 +28,8 @@ public class DocService {
     private DocMapper docMapper;
     @Resource
     private SnowFlake snowFlake;
-
+    @Resource
+    private ContentMapper contentMapper;
     public List<DocQueryResp> all() {
         DocExample docExample = new DocExample();
         docExample.setOrderByClause("sort asc");
@@ -57,13 +60,26 @@ public class DocService {
 
     public void save(DocSaveReq req) {
         Doc doc = CopyUtil.copy(req, Doc.class);
+        //这个copy方法使用的是Spring的BeanUtils的copyProperty,一般都是dao和dto
+        //之间使用的，就是说你的字段名称一样才能赋值，这里的参数是咋回事儿呢？
+        //就是把content拆分成一个对象，其实一个对象也行，但是这个字段的内容太大了。
+        //还是分开比较好，这里的设计就比较好了，请求参数就是用的专门的对象，和数据库
+        //的对象不是一模一样的，这个是全部的，但是在这里，我们可以获取到两个对象的
+        //内容，保存的时候就可以拆开了，就是对象是一个，里面存着两个对象的信息。
+        Content content = CopyUtil.copy(req, Content.class);
         if (ObjectUtils.isEmpty(req.getId())) {
             //新增
             doc.setId(snowFlake.nextId());
             docMapper.insert(doc);
+            content.setId(doc.getId());
+            contentMapper.insert(content);
         } else {
             //更新
             docMapper.updateByPrimaryKey(doc);
+            int count = contentMapper.updateByPrimaryKeyWithBLOBs(content);
+            if(count == 0){
+                contentMapper.insert(content);
+            }
         }
     }
     public void delete(Long id) {
