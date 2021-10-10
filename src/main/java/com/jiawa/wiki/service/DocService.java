55f8dfa20495fe3617e72eb78fbb5b120ea03f66
JiangTaoHou <925147954@qq.com>
2021-10-10 22:06:57 +0800

    阅读文档时，文档阅读数加1

--- src/main/java/com/jiawa/wiki/service/DocService.java
+++ src/main/java/com/jiawa/wiki/service/DocService.java
@@ -7,6 +7,7 @@ import com.jiawa.wiki.domain.Doc;
 import com.jiawa.wiki.domain.DocExample;
 import com.jiawa.wiki.mapper.ContentMapper;
 import com.jiawa.wiki.mapper.DocMapper;
+import com.jiawa.wiki.mapper.DocMapperCust;
 import com.jiawa.wiki.req.DocQueryReq;
 import com.jiawa.wiki.req.DocSaveReq;
 import com.jiawa.wiki.resp.DocQueryResp;
@@ -28,6 +29,8 @@ public class DocService {
     private DocMapper docMapper;
     @Resource
     private SnowFlake snowFlake;
+    @Resource
+    private DocMapperCust docMapperCust;
     @Resource
     private ContentMapper contentMapper;
     public List<DocQueryResp> all(Long ebookId) {
@@ -71,6 +74,8 @@ public class DocService {
         if (ObjectUtils.isEmpty(req.getId())) {
             //新增
             doc.setId(snowFlake.nextId());
+            doc.setViewCount(0);
+            doc.setVoteCount(0);
             docMapper.insert(doc);
             content.setId(doc.getId());
             contentMapper.insert(content);
@@ -94,6 +99,8 @@ public class DocService {
     }
     public String findContent(Long id) {
         Content content = contentMapper.selectByPrimaryKey(id);
+        //文档阅读数加1
+        docMapperCust.increaseViewCount(id);
         if(ObjectUtils.isEmpty(content)){
             return "";
         }else {
