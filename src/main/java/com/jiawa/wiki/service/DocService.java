d586c021667878b8cbd0e6be88702f98d98e0622
JiangTaoHou <925147954@qq.com>
2021-10-12 00:17:39 +0800

    首页显示电子书信息：文档数，点赞数，阅读数， doc/vote/**不需要拦截 增加日志流水号，方便运维

--- src/main/java/com/jiawa/wiki/service/DocService.java
+++ src/main/java/com/jiawa/wiki/service/DocService.java
@@ -130,5 +130,8 @@ public class DocService {
             throw new BusinessException(BusinessExceptionCode.VOTE_REPEAT);
         }
     }
+    public void updateEbookInfo(){
+        docMapperCust.updateEbookInfo();
+    }
 
 }
