4527dfc2b72e5fb45a50fb02ac8d6595506dba35
JiangTaoHou <925147954@qq.com>
2021-10-12 01:03:05 +0800

    完成点赞通知功能 点赞时，组装消息内容，往WS推送 前端收到WS消息后，弹出消息内容 注意：有时候你看到点赞之后不生效，可能就是session的问题，你重新部署一下前端页面，再重新打开一个页面，再点击，绝对没问题

--- src/main/java/com/jiawa/wiki/service/DocService.java
+++ src/main/java/com/jiawa/wiki/service/DocService.java
@@ -18,6 +18,7 @@ import com.jiawa.wiki.util.CopyUtil;
 import com.jiawa.wiki.util.RedisUtil;
 import com.jiawa.wiki.util.RequestContext;
 import com.jiawa.wiki.util.SnowFlake;
+import com.jiawa.wiki.websocket.WebSocketServer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -39,6 +40,8 @@ public class DocService {
     private ContentMapper contentMapper;
     @Resource
     private RedisUtil redisUtil;
+    @Resource
+    private WebSocketServer webSocketServer;
 
     public List<DocQueryResp> all(Long ebookId) {
         DocExample docExample = new DocExample();
@@ -129,6 +132,8 @@ public class DocService {
         }else {
             throw new BusinessException(BusinessExceptionCode.VOTE_REPEAT);
         }
+        Doc docDb = docMapper.selectByPrimaryKey(id);
+        webSocketServer.sendInfo("【"+docDb.getName()+"】被点赞");
     }
     public void updateEbookInfo(){
         docMapperCust.updateEbookInfo();
