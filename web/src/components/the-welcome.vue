f9940f68fa8aadeccd19d54cdb0ea5f80c52b9a7
JiangTaoHou <925147954@qq.com>
2021-10-14 13:29:56 +0800

    将欢迎文字提取成组件

---
+++ web/src/components/the-welcome.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>欢迎使用JAVA知识库111111</h1>
+
+</template>
+<script lang="ts">
+import {defineComponent} from 'vue';
+export default defineComponent({
+  name: 'the-welcome',
+  setup () {
+
+    return {
+
+    }
+  }
+});
+</script>
